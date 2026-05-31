721b3d39cafbc34beaf3b8c5492fdd00169ecb7c
mickmi <mick.miyg@gmail.com>
2026-05-31 23:15:49 +0800

    feat(nav): swap tab order to 赛事/活动/我的 + fix double-line in activity

--- miniprogram/custom-tab-bar/index.js
+++ miniprogram/custom-tab-bar/index.js
@@ -2,18 +2,18 @@ Component({
   data: {
     selected: 0,
     list: [
-      { pagePath: '/pages/index/index' },
       { pagePath: '/pages/match-list/match-list' },
+      { pagePath: '/pages/index/index' },
       { pagePath: '/pages/profile/profile' }
     ],
     inactiveIcons: [
-      '/assets/icons/tab-calendar.svg',
       '/assets/icons/tab-trophy.svg',
+      '/assets/icons/tab-calendar.svg',
       '/assets/icons/tab-person.svg'
     ],
     activeIcons: [
-      '/assets/icons/tab-calendar-active.svg',
       '/assets/icons/tab-trophy-active.svg',
+      '/assets/icons/tab-calendar-active.svg',
       '/assets/icons/tab-person-active.svg'
     ]
   },
--- miniprogram/pages/match-list/match-list.js
+++ miniprogram/pages/match-list/match-list.js
@@ -29,7 +29,7 @@ Page({
 
   onShow() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-      this.getTabBar().setData({ selected: 1 });
+      this.getTabBar().setData({ selected: 0 });
     }
     ensureRegistered().then(user => {
       if (!user) return;
